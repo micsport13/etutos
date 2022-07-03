96dc9d26b43c2a9dca08170a0890faa2a83dae69
micsport13 <micsport13@gmail.com>
2022-07-02 18:59:06 -0600

    Reformated intro message and refined pull_tos function

--- etutos.py
+++ etutos.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+
 
 def intro_message():
-    message = "Welcome to ETOTUS (Easy to Understand Terms of Service)! This tool is in active development, so please submit all issues or comments to the github repository at https://github.com/micsport13/etutos/issues. \n"
+    message = '''\
+    Welcome to ETOTUS! (Easy to Understand Terms of Service) 
+    This tool is in active development, so please submit all issues or comments to the github repository at 
+    https://github.com/micsport13/etutos/issues.'''
     print(message)
-def pull_tos():
-    url = input("Please enter url: ")
+
+
+def pull_tos(url):
+    title = re.search("[a-z]*(?=.com)", url).group(0).upper()
     content = requests.get(url).content
     try:
         test = requests.get(url)
@@ -18,30 +25,37 @@ def pull_tos():
         return 1
     except requests.exceptions.HTTPError:
         print("An HTTP Error occurred.  Please check the url.")
-    
-    headers = BeautifulSoup(content, 'html.parser').find_all('div', role = "article")
+
+    headers = BeautifulSoup(content, 'html.parser').find_all(
+        'div', role="article")
     paragraphs = BeautifulSoup(content, 'html.parser').find_all('p')
-    tos = ""
+    tos = title + "\n" + ""
     for p in paragraphs:
-        tos += p.get_text("\n",strip = True)
+        tos += p.get_text("\n", strip=True)
     return tos
 
-def tos_parser(tos):
-    tos = str(tos)
-    if tos.find("legal guardian")>= 0 or tos.find("parent")>=0:
+
+def tos_parser(tos: str):
+    if tos.find("legal guardian") >= 0 or tos.find("parent") >= 0:
         print("FIXME: Add what requirements for parental or guardian consent")
 
-def test():
+
+def input_url():
     url = input("Please enter url: ")
-    content = requests.get(url).content
-    
-    headers = BeautifulSoup(content, 'html.parser').find_all('div', id != "license", role = "article")
-    paragraphs = BeautifulSoup(content, 'html.parser').find_all('p')
-    bullet_points = BeautifulSoup(content, 'html.parser').find_all('li')
-    for p in headers:
-        print(p.get_text("\n", strip = True))
-    
+    if url.find("https://") == -1 and url.find("https://") == -1:
+        print("Please input a valid URL.")
+        input_url()
+    if url.find("terms") == -1:
+        print("Are you sure this is the address of the terms of service?")
+        response = input("Y or N: ")
+        if response.upper() == "Y":
+            return url
+        elif response.upper() == "N":
+            print("Please input a valid terms of service page: ")
+            input_url()
+    else:
+        return url
+
 
 intro_message()
-tos_parser(pull_tos())
-
+print(pull_tos(input_url()))
